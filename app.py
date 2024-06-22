7191ade110940bd61949b03b6b80e1071ea61bc6
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-06-22 11:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,10 @@
-# import streamlit as st
-# import pandas as pd
-# import plotly.graph_objects as go
-# import plotly.express as px
-
-# global df
-# df = pd.read_csv("sentiment_analysis.csv")
-# st.title('LECTURER SENTIMENT ANALYSIS')
-
-# def display_pie_chart(data):
-#     sentiment_counts = data['sentiment'].value_counts()
-#     fig = go.Figure(data=[go.Pie(labels=sentiment_counts.index, values=sentiment_counts.values, hole=0.3)])
-#     fig.update_layout(title=f'Sentiment Analysis for {selected_empname}', height=550, width=700)
-#     st.plotly_chart(fig, use_container_width=True)
-
-# def display_bar_chart(data):
-#     emp_sentiment_counts = data[data['sentiment'] == 'Positive']['empname'].value_counts(normalize=True).mul(100)
-#     colors = px.colors.qualitative.Set1  
-#     colors = colors * (len(emp_sentiment_counts) // len(colors)) + colors[:len(emp_sentiment_counts) % len(colors)] 
-#     fig = go.Figure(data=go.Bar(x=emp_sentiment_counts.index, 
-#                                  y=emp_sentiment_counts.values, 
-#                                  marker_color=colors))
-#     fig.update_layout(title='Positive Sentiment Percentage for All Employees', xaxis_title='Employee Name', yaxis_title='Positive Sentiment Percentage')
-#     st.plotly_chart(fig, use_container_width=True)
-
-# def display_sample_data(data):
-#     data = data.drop(columns=["_id", "question", "student_name", "campus_name", "course_name", "section_name"])
-#     with st.expander("Unique Comments by Sentiment", expanded=False):
-#         st.markdown("""
-#             <style>
-#             .scroll-table {
-#                 max-height: auto;
-#                 overflow: auto;
-#             }
-#             </style>
-#         """, unsafe_allow_html=True)
-#         st.write(data.sample(5).to_html(classes=['scroll-table'], escape=False), unsafe_allow_html=True)
-
-# chart_type = st.radio("", ('Comment-Analysis', 'Employee-Positive-Sentiment'))
-# option = st.sidebar.radio("","❓")
-
-# if chart_type == 'Comment-Analysis':
-#     st.sidebar.title('Filters')
-#     selected_empname = st.sidebar.selectbox('Select Employee Name:', df['empname'].unique())
-#     dept_code_options = df[df['empname'] == selected_empname]['departmentcode'].unique()
-#     selected_dept_code = st.sidebar.selectbox('Select Department Code:', dept_code_options)
-#     section_options = df[(df['empname'] == selected_empname) & (df['departmentcode'] == selected_dept_code)]['course_name'].unique()
-#     selected_course = st.sidebar.selectbox('Select section :', section_options)
-
-#     filtered_df = df[(df['empname'] == selected_empname) & 
-#                     (df['departmentcode'] == selected_dept_code) & 
-#                     (df['course_name'] == selected_course)]
-
-#     display_pie_chart(filtered_df)
-#     display_sample_data(filtered_df)
-
-# elif chart_type == 'Employee-Positive-Sentiment':
-#     display_bar_chart(df)
-    
-# elif chart_type == "❓":
-
-
-#     st.markdown("""
-#     <div style="display:flex; justify-content:center; align-items:center; height:800px;">
-#         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSS9ty8udh3_qGB5OEFKafsCXBfAQPdnsgj24j0yuktRIICd4VRECTQ6QSeKIoSzyNpztxc2as_F37A/embed?start=false&loop=false&delayms=3000" 
-#         width="1000" height="450"></iframe>
-#     </div>
-#     """, unsafe_allow_html=True)
-
-
-
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 
 global df
-df = pd.read_csv("sentiment_analysis.csv")
+df = pd.read_csv("feedbackData.csv")
 st.title('LECTURER SENTIMENT ANALYSIS')
 
 def display_pie_chart(data):
@@ -85,7 +14,7 @@ def display_pie_chart(data):
     st.plotly_chart(fig, use_container_width=True)
 
 def display_bar_chart(data):
-    emp_sentiment_counts = data[data['sentiment'] == 'Positive']['empname'].value_counts(normalize=True).mul(100)
+    emp_sentiment_counts = data[data['sentiment'] == 'positive']['empname'].value_counts(normalize=True).mul(100)
     colors = px.colors.qualitative.Set1  
     colors = colors * (len(emp_sentiment_counts) // len(colors)) + colors[:len(emp_sentiment_counts) % len(colors)] 
     fig = go.Figure(data=go.Bar(x=emp_sentiment_counts.index, 
@@ -95,7 +24,7 @@ def display_bar_chart(data):
     st.plotly_chart(fig, use_container_width=True)
 
 def display_sample_data(data):
-    data = data.drop(columns=["_id", "question", "student_name", "campus_name", "course_name", "section_name"])
+    data = data.drop(columns=[ "question", "student_name", "campus_name", "course_name", "section_name"])
     with st.expander("Unique Comments by Sentiment", expanded=False):
         st.markdown("""
             <style>
@@ -115,29 +44,46 @@ def display_presentation():
     </div>
     """, unsafe_allow_html=True)
 
-button_clicked = False
-button_clicked = st.button("ABOUT")
-chart_type = st.radio("", ('Comment-Analysis', 'Employee-Positive-Sentiment'),index=None)
-
-    
-
-if chart_type == 'Comment-Analysis':
-    st.sidebar.title('Filters')
-    selected_empname = st.sidebar.selectbox('Select Employee Name:', df['empname'].unique())
-    dept_code_options = df[df['empname'] == selected_empname]['departmentcode'].unique()
-    selected_dept_code = st.sidebar.selectbox('Select Department Code:', dept_code_options)
-    section_options = df[(df['empname'] == selected_empname) & (df['departmentcode'] == selected_dept_code)]['course_name'].unique()
-    selected_course = st.sidebar.selectbox('Select section :', section_options)
-
-    filtered_df = df[(df['empname'] == selected_empname) & 
-                    (df['departmentcode'] == selected_dept_code) & 
-                    (df['course_name'] == selected_course)]
-
-    display_pie_chart(filtered_df)
-    display_sample_data(filtered_df)
-
-elif chart_type == 'Employee-Positive-Sentiment':
-    display_bar_chart(df)
-
-if button_clicked:
+st.sidebar.title('ABOUT❓')
+show_presentation = False
+if st.sidebar.button("ABOUT❓"):
+    show_presentation = not show_presentation
+if show_presentation:
     display_presentation()
+else:
+    chart_type = st.radio("Select Chart Type:", ('Comment-Analysis', 'Employee-Positive-Sentiment'))
+
+    if chart_type == 'Comment-Analysis':
+        st.sidebar.title('Filters')
+        selected_empname = st.sidebar.selectbox('Select Employee Name:', df['empname'].unique())
+        dept_code_options = df[df['empname'] == selected_empname]['department_code'].unique()
+        selected_dept_code = st.sidebar.selectbox('Select Department Code:', dept_code_options)
+        section_options = df[(df['empname'] == selected_empname) & (df['department_code'] == selected_dept_code)]['course_name'].unique()
+        selected_course = st.sidebar.selectbox('Select section :', section_options)
+
+        filtered_df = df[(df['empname'] == selected_empname) & 
+                        (df['department_code'] == selected_dept_code) & 
+                        (df['course_name'] == selected_course)]
+
+        display_pie_chart(filtered_df)
+        display_sample_data(filtered_df)
+        st.subheader("About Creator")
+        with st.expander("Kalyan Kanchumarthi"):
+                col1, col2 = st.columns([1, 3])  # Adjust the ratio as needed
+                with col1:
+                    st.image("mypic.jpg", use_column_width=True)
+                with col2:
+                    st.write("""
+                    Hello! I'm Kalyan Kanchumarthi, \n
+                    a passionate developer exploring the world of AI and programming.
+
+                    - I love building applications that make life easier.
+                    - I'm good at Python and data analysis.
+                    - Don't misunderstand me as a nerd; I'm socially adept too! 😄
+                    - Thank you for checking out my app!
+
+                    Do check out my [LinkedIn](https://www.linkedin.com/in/kalyan-kanchumarthi-a6320a235/) and [GitHub](https://github.com/SrinivasaKalyan).
+                    """)
+
+    elif chart_type == 'Employee-Positive-Sentiment':
+        display_bar_chart(df)
