a993f78cdcf33ef31b144d3e3e1f2d2432335db7
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-07-19 09:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 st.set_page_config(
     page_title="TeachTrack",
-    page_icon="chart.png", 
+    page_icon="https://www.flaticon.com/free-icon/emotional_12649758", 
     initial_sidebar_state="auto"
 )
 
