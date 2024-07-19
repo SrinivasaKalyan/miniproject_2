5595f8960397b7e57fe60cc01d04d18e6ea9f6e8
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-07-19 09:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 st.set_page_config(
     page_title="TeachTrack",
-    page_icon="mood.gif", 
+    page_icon="sentiment-analysis.png", 
     initial_sidebar_state="auto"
 )
 
